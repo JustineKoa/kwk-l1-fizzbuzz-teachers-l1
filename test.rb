a4fb590667a2d7bedd1f4fb8f83c9c5945b3ef61
Justine Koa <justinekoa@gmail.com>
2018-08-07 20:08:46 +0000

    automatically backed up by learn

---
+++ test.rb
@@ -0,0 +1,14 @@
+# items = {}
+# items["zero"] = 0
+# items["one"] = 1
+#
+# puts items
+
+tops = ["blouses", "t-shirts", "jackets"]
+bottoms = ["jeans", "skirts", "shorts"]
+footwear = ["sandals", "sneakers", "heels"]
+
+clothes = [tops, bottoms, footwear]
+
+footwear << "birkenstocks"
+# puts clothes
